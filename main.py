a767299a96dd5c18f51e8ea5e062796b8cfafaa5
Laszlo Kopits <laszlo.kopits@yale.edu>
2020-04-30 17:58:25 -0400

    added cmd-line functionality to use file images instead of webcam

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import cv2
 import numpy as np
 import mazeSolve
+import sys
 
-cap = cv2.VideoCapture(0)
+print(sys.argv)
 
-while(True):
-    ret, frame = cap.read()
-    frame = cv2.flip(frame, 1)
+if len(sys.argv) > 1:
+    mazeSolve.solve(cv2.imread(sys.argv[1]))
+else:
+    cap = cv2.VideoCapture(0)
 
-    cv2.imshow('frame', frame)
+    while(True):
+        ret, frame = cap.read()
+        frame = cv2.flip(frame, 1)
+        cv2.imshow('frame', frame)
 
-    key = cv2.waitKey(1)
-    if key == ord('q'):
-        break
-    elif key == ord('s'):
-        mazeSolve.solve(frame)
-
-cap.release()
-cv2.destroyAllWindows()
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            break
+        elif key == ord('s'):
+            mazeSolve.solve(frame)
+    cap.release()
+    cv2.destroyAllWindows()
